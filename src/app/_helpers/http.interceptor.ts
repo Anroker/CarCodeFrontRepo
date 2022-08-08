a9c9ff29eb63ffd108f2a1950534a4f226622244
Anrok <anrok1234@gmail.com>
2022-08-09 00:50:44 +0200

    Initialize of Front end for application

---
+++ src/app/_helpers/http.interceptor.ts
@@ -0,0 +1,30 @@
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from './token-storage.service';
+
+const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
+@Injectable()
+//Change name of class
+export class HttpRequestInterceptor implements HttpInterceptor {
+    constructor(private token: TokenStorageService) { }
+    
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let authReq = req;
+        const token = this.token.getToken();
+
+        if (token != null) {
+            authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
+          }
+
+        // req = req.clone({
+        //     withCredentials: true,
+        // });
+        return next.handle(authReq);
+    }
+}
+
+export const HttpInterceptorProviders = [{
+    provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true
+},
+]
---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { StorageService } from './_services/storage.service';
+import { AuthService } from './_services/auth.service';
+import { TokenStorageService } from './_helpers/token-storage.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CarCodeFront';
+
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+  constructor(private tokenStorageService: TokenStorageService) { }
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      // this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      // this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+      this.username = user.username;
+    }
+  }
+
+  logout(): void {
+
+    this.tokenStorageService.signOut();
+
+    // this.authService.logout().subscribe({
+    //   next: res => {
+    //     console.log(res);
+    //     this.storageService.clean();
+    //   },
+    //   error: err => {
+    //     console.log(err);
+    //   }
+    // });
+    
+    window.location.reload();
+  }
+}
